fb0b21c99f0a496255c2840453278d868bf758d3
tbfh <2986579627@qq.com>
2024-05-30 14:40:53 +0800

    20240530

--- js/script.js
+++ js/script.js
@@ -31,3 +31,19 @@ navLinks.forEach(function(link) {
         }
     });
 });
+
+
+// axios({
+//     method:'get',
+//     url: 'http://121.36.244.57:5244/d/AliDrive/SmartCampus/Walkthrough.mp4?sign=IjtfLX3nM2d-WtZQD4YMKijurhtAKGWPAYD5J9l1uvA=:0',
+//     responseType: 'blob',
+// }).then(res=>{
+//     console.log(res);
+//     console.log("Fired!!");
+//     let blob = new Blob([res.data],{type:res.data.type});
+//     let url = URL.createObjectURL(blob);
+//     document.querySelector('video').src = url;
+// }).catch(function(err){
+//     console.log("Failed");
+// });
+
